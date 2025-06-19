0129d476752dd04d60fdf8d47c2a1f8d64cff507
Ranizt <chicuongvo3@gmail.com>
2025-06-19 18:20:57 +0700

    Add full viewport height

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <div className="font-primary dark:bg-gray-800 ">
       <ScrollToTop />
       <Navbar />
-      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <main className="max-w-7xl min-h-dvh mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <Outlet />
       </main>
       <ToastContainer theme="colored" />
