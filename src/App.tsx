ac3447f86e80e22cdf7f6b144c52b005ea92fd52
unknown <ungquangtri@gmail.com>
2025-06-19 22:51:08 +0700

    Add chatbot

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router";
 import Navbar from "./components/Client/Navbar";
 import ScrollToTop from "./components/ScrollToTop";
 import { ToastContainer } from "react-toastify";
+import Chatbot from "./pages/Client/Chatbot";
 function App() {
   return (
     <div className="font-primary ">
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <Outlet />
+        <Chatbot />
       </main>
       <ToastContainer />
     </div>
